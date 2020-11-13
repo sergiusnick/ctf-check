b7df3da9bd8e3c71cf5300be44fdc502b2b93821
sergiusnick <34229611+sergiusnick@users.noreply.github.com>
2020-11-13 22:10:50 +0500

    main (#2)
    Co-authored-by: sergiusnick <sergiusnick@yandex-team.ru>

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+a_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+
+try:
+    1 / 0
+except:
+    pass
+
+
 def print_env():
     for i in os.environ:
         logger.exception(f'k={i}, v={os.environ[i]}')
