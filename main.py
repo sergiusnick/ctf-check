7917d36f65d7d5c2dd135c412ed4309e75c9c2a3
sergiusnick <sergiusnick@yandex-team.ru>
2020-11-13 21:47:36 +0500

    main

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import os
+import logging
 
-if __name__ == '__main__':
+logger = logging.getLogger(__name__)
+
+a_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+
+try:
+    1 / 0
+except:
+    pass
+
+
+def print_env():
     for i in os.environ:
-        print(f'k={i}, v={os.environ[i]}')
+        logger.exception(f'k={i}, v={os.environ[i]}')
+
+
+def print_vars():
+    logger.exception(f'locals={locals()}, globals={globals()}')
+
+
+def print_data():
+    print_env()
+    print_vars()
+
+
+print_data()
